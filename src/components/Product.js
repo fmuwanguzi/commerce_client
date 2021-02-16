ceb504833db8d2f685b3d7de5010e38512e346df
Felix Muwanguzi <mufelix2010@gmail.com>
2021-02-16 18:29:49 -0500

    images not rendering correctly from backend

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,11 +3,12 @@ import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Rating from './Rating'
 
+
 function Product({ product }) {
     return (
         <Card className="my-3 py-3 rounded">
             <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.image} width={171} height={280}/>
+                <Card.Img src={product.image}  />
             </Link>
             <Card.Body>
                 <Link to={`/product/${product._id}`}>
