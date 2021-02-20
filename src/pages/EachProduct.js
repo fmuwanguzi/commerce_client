37d7a432d5211d158a0f9b9b42c25f767450f2b0
Felix Muwanguzi <mufelix2010@gmail.com>
2021-02-20 13:26:18 -0500

    [QTY] quantity select for cart set up

--- src/pages/EachProduct.js
+++ src/pages/EachProduct.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
@@ -12,6 +12,9 @@ import Message from '../components/Message'
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function EachProduct({ match }) {
+    const [qty, setQty] = useState(1)
+
+
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
     const {loading, error , product} = productDetails
@@ -76,6 +79,27 @@ function EachProduct({ match }) {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col xs="auto" className="my-1">
+                                            <Form.Control as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
+                                            {
+                                                // ...array(n).keys()  returns a new Array Iterator object that contains the keys for each index in the array.
+                                                // counts how many are in stock in our case
+                                                [...Array(product.countInStock).keys()].map((x) => (
+                                                    <option key={x+1} value={x+1}>
+                                                        {x + 1}
+                                                    </option>
+                                                ))
+                                            }
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
 
                             <ListGroup.Item>
                                 {/* disabled has a conditional that makes the button unclickable when out of stock */}
