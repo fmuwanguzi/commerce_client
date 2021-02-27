4ccf30a22f71f963d23a6b325fd36a56580ccdc0
Felix Muwanguzi <mufelix2010@gmail.com>
2021-02-27 15:16:22 -0500

    cart remove item button is working

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message  from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 
 function Cart({ match, location, history }) {
@@ -29,7 +29,7 @@ function Cart({ match, location, history }) {
 
 
     const removeFromCartHandler = (id) => {
-        console.log('remove', id)
+        dispatch(removeFromCart(id))
     }
 
     const checkoutHandler = () => {
