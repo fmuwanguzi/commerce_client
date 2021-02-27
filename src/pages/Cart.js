2dfe073b361b279154a8b66c3775e7a7ab1dca22
Felix Muwanguzi <mufelix2010@gmail.com>
2021-02-27 14:58:41 -0500

    cart page - buttons added for remove item and proceed to checkout

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,7 +6,6 @@ import Message  from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
 
-
 function Cart({ match, location, history }) {
     const productId = match.params.id
     
@@ -28,31 +27,93 @@ function Cart({ match, location, history }) {
         }
     }, [dispatch, productId, qty])
 
+
+    const removeFromCartHandler = (id) => {
+        console.log('remove', id)
+    }
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
+    }
+
     return (
         <Row>
             <Col md={8}>
                 <h1>Shopping Cart</h1>
                 {cartItems.length === 0 ? (
                     <Message variant='primary'>
-                        Your cart is empty <Link to='/'> head Home</Link>
+                        Your cart is empty <Link to='/' >Head Home</Link>
                     </Message>
-                ): (
-                    <ListGroup variant='flush'>
-                        {cartItems.map(item => {
-                            <ListGroup.Item key={item.product}>
-                                <Row>
-                                    <Col md={2}>
-                                        <Image src={item.Image}/>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                        })}
-                    </ListGroup>
-                )}
+                ) : (
+                        <ListGroup variant='flush'>
+                            {cartItems.map(item => {
+                               return  <ListGroup.Item key={item.product}>
+                                            <Row>
+                                                <Col md={2}>
+                                                    <Image src={item.image} alt={item.name} fluid rounded/>
+                                                </Col>
+                                                
+                                                <Col md={3}>
+                                                    <Link to={`/product/${item.product}`}> {item.name} </Link>
+                                                </Col>
+                                                
+                                                <Col md={2}>
+                                                    $ {item.price}
+                                                </Col>
+
+                                                <Col md={3}>
+                                                    <Form.Control 
+                                                        as="select" 
+                                                        value={item.qty} 
+                                                        onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                                                        {
+                                                            // ...array(n).keys()  returns a new Array Iterator object that contains the keys for each index in the array.
+                                                            // counts how many are in stock in our case
+                                                            [...Array(item.countInStock).keys()].map((x) => (
+                                                                <option key={x+1} value={x+1}>
+                                                                    {x + 1}
+                                                                </option>
+                                                            ))
+                                                        }
+                                                    </Form.Control>
+                                                </Col>
+                                                
+                                                <Col md={1}>
+                                                    <Button 
+                                                    type='button'
+                                                    variant = 'light'
+                                                    onClick={() => removeFromCartHandler(item.product)}
+                                                    >
+                                                        <i className='fas fa-trash-alt'> Remove item </i>
+                                                    </Button>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                            })}
+                        </ListGroup>
+                    )}
             </Col>
             
             <Col md={4}>
-                
+                <Card>
+                    <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h2>SUBTOTAL ({cartItems.reduce((acc, item) => acc + item.qty, 0 )}) items </h2>
+                                 $ {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2) }                              
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Button
+                                type='button'
+                                className='btn-block'
+                                disabled={cartItems.length === 0 }
+                                onClick={checkoutHandler}
+                                >
+                                Proceed to checkout
+                                </Button>
+                            </ListGroup.Item>
+                    </ListGroup>
+                </Card>
             </Col>
         </Row>
     )
